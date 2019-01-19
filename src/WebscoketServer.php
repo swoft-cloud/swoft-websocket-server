981be2799af6450eafb1044f7e8197d2cd695777
stelin <794774870@qq.com>
2019-01-19 20:51:43 +0800

    add listener

--- src/WebscoketServer.php
+++ src/WebscoketServer.php
@@ -1,8 +1,26 @@
 <?php
+
 namespace Swoft\WebSocket\Server;
 
 
-class WebscoketServer
-{
+use Swoft\Bean\Annotation\Mapping\Bean;
+use Swoft\Server\Server;
 
+/**
+ * Class WebscoketServer
+ *
+ * @Bean("wsServer")
+ *
+ * @since 2.0
+ */
+class WebscoketServer extends Server
+{
+    /**
+     * Start
+     */
+    public function start(): void
+    {
+        $this->swooleServer = new \Co\Websocket\Server($this->host, $this->port);
+        $this->startSwoole();
+    }
 }
