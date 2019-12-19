f06388342f23f3d815c2261014ce224d9749b654
inhere <in.798@qq.com>
2019-12-20 00:42:45 +0800

    refatcing session connection stroage for ws,tcp

--- src/ConnectionManager.php
+++ src/ConnectionManager.php
@@ -2,7 +2,29 @@
 
 namespace Swoft\WebSocket\Server;
 
-class ConnectionManager
-{
+use Swoft\Bean\Annotation\Mapping\Bean;
+use Swoft\Contract\SessionInterface;
+use Swoft\Concern\AbstractSessionManager;
+use Swoft\Stdlib\Helper\JsonHelper;
 
+/**
+ * Class ConnectionManager
+ *
+ * @since 2.0.8
+ * @Bean("wsConnectionManager")
+ */
+class ConnectionManager extends AbstractSessionManager
+{
+    /**
+     * @param string $sessionData
+     *
+     * @return SessionInterface
+     */
+    protected function restoreSession(string $sessionData): SessionInterface
+    {
+        /** @var SessionInterface $class */
+        $data  = JsonHelper::decode($sessionData, true);
+
+        return Connection::newFromArray($data);
+    }
 }
